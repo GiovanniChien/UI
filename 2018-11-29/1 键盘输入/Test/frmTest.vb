e10daf3202f4dccc77eb5c4684b72e16c0c591ae
Oliver <1300687449@qq.com>
2018-12-02 21:51:44 +0800

    2018-12-2

---
+++ 2018-11-29/1 键盘输入/Test/frmTest.vb
@@ -0,0 +1,35 @@
+﻿Public Class frmTest
+
+    Dim Words As List(Of String) = New List(Of String)
+
+    Private Sub frmTest_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim strWords() As String = {"abcd", "bcd", "cd"}
+        Words.AddRange(strWords)
+    End Sub
+
+
+    Private Sub txtContaint_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtContaint.KeyPress
+        Dim lastchar As Char
+        If txtContaint.SelectionStart = 0 Then
+            lastchar = ""
+        Else
+            lastchar = txtContaint.Text(txtContaint.SelectionStart - 1)
+        End If
+        Me.Text = lastchar
+        Dim k As Integer = Find(e.KeyChar)
+        If k <> -1 Then
+            e.KeyChar = ""
+            txtContaint.SelectedText = Words(k) & " "
+        End If
+    End Sub
+
+    Private Function Find(ByVal keychar As Char) As Integer
+        For i = 0 To Words.Count - 1
+            If Words(i).StartsWith(keychar) Then
+                Return i
+            End If
+        Next
+        Return -1
+    End Function
+
+End Class
---
+++ 2018-11-29/2 词库类/Test/frmListBox.vb
@@ -0,0 +1,18 @@
+﻿Imports System.IO
+
+Public Class frmListBox
+    Dim Words As List(Of String) = New List(Of String)
+    Private LastChars As String
+    Public Event WordSelected(ByVal Word As String)
+    Public Event CharPress(ByVal C As Char)
+
+    Private Sub frmListBox_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim content As String
+        Using fs As New IO.FileStream("Words.txt", FileMode.Open)
+            Using sr As New StreamReader(fs, System.Text.Encoding.Default)
+                content = sr.ReadToEnd()
+            End Using
+        End Using
+
+    End Sub
+End Class
